659ba162184d09c36f04a3c716e006bb206a9d39
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-11-12 15:48:54 +0700

    Update map0911.cpp

--- map0911.cpp
+++ map0911.cpp
@@ -2,56 +2,84 @@
 #include <map>
 using namespace std;
 
-/*string arabicToRoman(int num) {
-    //string result = ""
-
-   map<int, string> romanNumerals{
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-    };
-
-    for (auto it = romanNumerals.begin(); it != romanNumerals.end(); ++it) {
-        while (num >= it->first) {
-            result += it->second;
-            num -= it->first;
-        }
-    }
-
-    return result;
-} */
-
-int romanToArabic(string roman) {
+// Функция для перевода римской цифры в арабскую
+int romanToArabic(string roman)
+{
+    map<char, int> romanMap;
+    romanMap['I'] = 1;
+    romanMap['V'] = 5;
+    romanMap['X'] = 10;
+    romanMap['L'] = 50;
+    romanMap['C'] = 100;
+    romanMap['D'] = 500;
+    romanMap['M'] = 1000;
+    
     int result = 0;
-
-    map<char, int> romanNumerals{
-        {'M', 1000}, {'D', 500}, {'CD', 400}, {'C', 100}, {"XC", 90}, {'L', 50}, {'XL', 40}, {'X', 10}, {"IX", 9}, {'V', 5}, {'IV',4}, {'I', 1}
-    };
-
-    for (int i = 0; i < roman.length(); i++) {
-        if (i < roman.length() - 1 && romanNumerals[roman[i]] < romanNumerals[roman[i + 1]]) {
-            result -= romanNumerals[roman[i]];
+    int prevValue = 0;
+    
+    for (int i = roman.length() - 1; i >= 0; i--) {
+        int currentValue = romanMap[roman[i]];
+        
+        if (currentValue < prevValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
         }
-        else {
-            result += romanNumerals[roman[i]];
-        }
+
+        prevValue = currentValue;
     }
-
+    
     return result;
 }
 
-int main() {
-    int arabicNumber;
-    string romanNumber;
+// Функция для перевода арабской цифры в римскую
+string arabicToRoman(int arabic)
+{
+    map<int, string> romanMap;
+    romanMap[1] = "I";
+    romanMap[4] = "IV";
+    romanMap[5] = "V";
+    romanMap[9] = "IX";
+    romanMap[10] = "X";
+    romanMap[40] = "XL";
+    romanMap[50] = "L";
+    romanMap[90] = "XC";
+    romanMap[100] = "C";
+    romanMap[400] = "CD";
+    romanMap[500] = "D";
+    romanMap[900] = "CM";
+    romanMap[1000] = "M";
+    
+    string result;
+    
+    for (auto it = romanMap.rbegin(); it != romanMap.rend(); ++it) {
+        int currentValue = it->first;
+        string numeral = it->second;
+        
+        while (arabic >= currentValue) {
+            result += numeral;
+            arabic -= currentValue;
+        }
+    }
+    
+    return result;
+}
 
-    cin >> arabicNumber;
-    cin >> romanNumber;
-
-    string romanResult = arabicToRoman(arabicNumber);
-    cout << arabicNumber << " = " << romanResult << endl;
-
-    int arabicResult = romanToArabic(romanNumber);
-    cout << romanNumber << " = " << arabicResult << endl;
-
+int main()
+{
+    string input;
+    int number;
+    
+    cout << "Введите римскую цифру или арабское число: ";
+    cin >> input;
+    
+    // Проверка, является ли ввод римской цифрой или арабским числом
+    if (isdigit(input[0])) {
+        number = stoi(input);
+        cout << "Арабская цифра " << number << " в римской системе: " << arabicToRoman(number) << endl;
+    } else {
+        cout << "Римская цифра " << input << " в арабской системе: " << romanToArabic(input) << endl;
+    }
+    
     return 0;
 }
